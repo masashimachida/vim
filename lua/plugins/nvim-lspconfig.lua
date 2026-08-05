e3f9f74a7b675c9c6160da6e9676ec8a4df697de
Masashi MACHIDA <masashi.machida@alfasado.jp>
2026-08-05 13:41:45 +0900

    lazydev.nvim追加

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -5,6 +5,15 @@ return
 		"williamboman/mason.nvim",
 		"williamboman/mason-lspconfig.nvim",
 		"hrsh7th/cmp-nvim-lsp",
+        {
+			"folke/lazydev.nvim",
+			ft = "lua",
+			opts = {
+				library = {
+					{ path = "${3rd}/luv/library", words = { "vim%.uv" } },
+				},
+			},
+		},
 	},
 	config = function()
 		require("mason").setup()
