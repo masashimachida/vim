e7fcedc5adf6e6be2b46c9558ca4eb9993d218f9
Masashi MACHIDA <masashi.machida@alfasado.jp>
2026-08-17 12:14:01 +0900

    fix: dboutバッファ離脱時にfoldenableを元に戻す
    クエリ結果が折りたたまれて表示される不具合を修正。dbout.vimが
    foldmethod=exprを設定するため、複数回クエリを実行すると2つ目以降の
    結果セットが折りたたまれたままになる問題への対処。

--- lua/plugins/vim-dadbod-ui.lua
+++ lua/plugins/vim-dadbod-ui.lua
@@ -13,5 +13,29 @@ return {
   init = function()
     -- Your DBUI configuration
     vim.g.db_ui_use_nerd_fonts = 1
+
+    -- クエリ結果が折りたたまれて表示されるのを防ぐ
+    -- (dbout.vimがfoldmethod=exprを設定し、複数回クエリを実行すると
+    --  2つ目以降の結果セットが折りたたまれたままになるため)
+    -- foldenableはウィンドウローカルなため、dboutバッファを離れる際に
+    -- 元に戻さないと、同じウィンドウで開いた別の通常ファイルにも
+    -- foldenable=falseが引き継がれてしまう
+    local dbout_augroup = vim.api.nvim_create_augroup("DbUiDboutFold", { clear = true })
+    vim.api.nvim_create_autocmd("FileType", {
+      group = dbout_augroup,
+      pattern = "dbout",
+      callback = function()
+        vim.opt_local.foldenable = false
+      end,
+    })
+    vim.api.nvim_create_autocmd("BufWinLeave", {
+      group = dbout_augroup,
+      pattern = "*",
+      callback = function(args)
+        if vim.bo[args.buf].filetype == "dbout" then
+          vim.wo.foldenable = true
+        end
+      end,
+    })
   end,
 }
